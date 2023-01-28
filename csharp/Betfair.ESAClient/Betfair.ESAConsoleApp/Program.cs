5b3273e6867fa371d4f53d14aee590e1f1694b1b
Pete Buckney <peter.buckney@gmail.com>
2023-01-28 13:06:52 +0000

    Move all projects from .NET Framework to .NET Core
    Two things incomplete:
    - Betfair.ESAConsoleApp does not build - problems with Settings and NCommand
    - ClientTests do not run, as they currently lock the port on the dummy TCP server

--- csharp/Betfair.ESAClient/Betfair.ESAConsoleApp/Program.cs
+++ csharp/Betfair.ESAClient/Betfair.ESAConsoleApp/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
+﻿using System.Diagnostics;
 using Betfair.ESAClient;
 using Betfair.ESAClient.Auth;
 using Betfair.ESAClient.Cache;
